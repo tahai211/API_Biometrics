0931a5a04ac49f29173416ac8d211ae6c5d6fb72
tahai211 <taiich197@gmail.com>
2024-07-14 22:16:14 +0700

    commit

---
+++ MockUp_CardZ/DTO/Entity/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockUp_CardZ.DTO.Entity
+{
+    internal class UserEntity
+    {
+    }
+}
